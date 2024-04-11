9f5591ec60c63fc03d15cd799446d115021ffa4c
ganeshkokkera <ganeshkokkera666@gmail.com>
2024-04-11 16:39:53 -0500

    Added UI using Angular Framework

--- API/Program.cs
+++ API/Program.cs
@@ -10,11 +10,15 @@ builder.Services.AddDbContext<DataContext> ( opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 
 var app = builder.Build();
 
 
 app.UseHttpsRedirection();
+
+// COnfigure the Http pipeline
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
 app.UseAuthorization();
 
