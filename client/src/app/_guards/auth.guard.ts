fbdd18c6d758ded0973c6c19994b9fa1071ca8c8
ganeshkokkera <ganeshkokkera666@gmail.com>
2024-05-07 12:40:26 -0500

    Added Routing in client side

---
+++ client/src/app/_guards/auth.guard.ts
@@ -0,0 +1,22 @@
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { AccountService } from '../_services/account.service';
+import { ToastrService } from 'ngx-toastr';
+import { map } from 'rxjs';
+
+export const authGuard: CanActivateFn = (route, state) => {
+  const accountService = inject(AccountService);
+  const toastr = inject(ToastrService);
+  const router = inject(Router)
+
+  return accountService.currentUser$.pipe(
+    map(user => {
+      if (user) return true;
+      else {
+        toastr.error('Access Denied! Please login first');
+        router.navigateByUrl('/')
+        return false;
+      }
+    })
+  )
+};
